16f9989f725c27cf1dfab75174f916d6d9e65644
Feeeeddmmmeee <69256898+Feeeeddmmmeee@users.noreply.github.com>
2026-03-07 14:58:24 +0100

    implemented a pipeline abstraction + pipeline cache/manager

---
+++ include/Pipeline.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
+
+#include <unordered_map>
+
+struct PipelineKey
+{
+	std::string vertMain;
+	std::string fragMain;
+	std::string vert;
+	std::string frag;
+
+	bool operator==(const PipelineKey&) const = default;
+};
+
+namespace std {
+    template<> struct hash<PipelineKey> {
+        size_t operator()(PipelineKey const& pipeline) const {
+			size_t hsh = 0;
+
+			auto combine = [&hsh](const std::string &s) {
+				return (hsh ^ (hash<string>{}(s) << 1)) >> 1;
+			};
+
+			combine(pipeline.vert);
+			combine(pipeline.frag);
+			combine(pipeline.vertMain);
+			combine(pipeline.fragMain);
+			return hsh;
+        }
+    };
+}
+
+struct VulkanPipeline
+{
+	vk::raii::Pipeline pipeline = nullptr;
+	vk::raii::PipelineLayout layout = nullptr;
+
+	VulkanPipeline(const vk::raii::Device &dev, const PipelineKey &key, const vk::SurfaceFormatKHR &surfaceFormat, vk::Format depthFormat, const vk::raii::DescriptorSetLayout &descSetLayout);
+};
+
+struct Material
+{
+	std::shared_ptr<VulkanPipeline> pipeline = nullptr;
+
+	void setPipeline(std::shared_ptr<VulkanPipeline> pipeline)
+	{
+		this->pipeline = pipeline;
+	}
+};
+
+class PipelineManager
+{
+	public:
+		PipelineManager(const vk::raii::Device &dev, const vk::raii::DescriptorSetLayout &layout, vk::Format depth, const vk::SurfaceFormatKHR &surface) : 
+			device(dev), descSetLayout(layout), depthFormat(depth), surfaceFormat(surface){}
+		std::shared_ptr<VulkanPipeline> get(const PipelineKey &key);
+
+	private:
+		vk::Format depthFormat;
+		const vk::SurfaceFormatKHR &surfaceFormat;
+		const vk::raii::DescriptorSetLayout &descSetLayout;
+		const vk::raii::Device &device;
+
+		std::unordered_map<PipelineKey, std::shared_ptr<VulkanPipeline>> cache;
+
+		std::shared_ptr<VulkanPipeline> createPipeline(const PipelineKey &key);
+};
---
+++ src/Pipeline.cpp
@@ -0,0 +1,114 @@
+#include "Pipeline.h"
+#include "Shader.h"
+#include "Vertex.h"
+
+VulkanPipeline::VulkanPipeline(const vk::raii::Device &dev, const PipelineKey &key, const vk::SurfaceFormatKHR &surfaceFormat, vk::Format depthFormat, const vk::raii::DescriptorSetLayout &descSetLayout)
+{
+	// modules should probably be cached at some point
+
+	VulkanShader vert(dev, key.vert);
+	VulkanShader frag(dev, key.frag);
+	vk::PipelineShaderStageCreateInfo vertInfo{
+		.stage = vk::ShaderStageFlagBits::eVertex,
+			.module = vert.module,
+			.pName = key.vertMain.c_str()
+	};
+	vk::PipelineShaderStageCreateInfo fragInfo{
+		.stage = vk::ShaderStageFlagBits::eFragment,
+			.module = frag.module,
+			.pName = key.fragMain.c_str()
+	};
+	vk::PipelineShaderStageCreateInfo shaderStages[] = {vertInfo, fragInfo};
+
+	auto bindingDescription = Vertex::getBindingDesc();
+	auto attrDescriptions = Vertex::getAttributeDescs();
+	vk::PipelineVertexInputStateCreateInfo vertInputInfo{
+		.vertexBindingDescriptionCount=1,
+			.pVertexBindingDescriptions=&bindingDescription,
+			.vertexAttributeDescriptionCount=attrDescriptions.size(),
+			.pVertexAttributeDescriptions=attrDescriptions.data()
+	};
+
+	vk::PipelineInputAssemblyStateCreateInfo inputAssembly{.topology=vk::PrimitiveTopology::eTriangleList};
+	vk::PipelineViewportStateCreateInfo viewportState{.viewportCount = 1, .scissorCount = 1};
+	std::vector dynamicStates = {
+		vk::DynamicState::eViewport,
+		vk::DynamicState::eScissor
+	};
+	vk::PipelineDynamicStateCreateInfo dynamicState{
+		.dynamicStateCount = (uint32_t)dynamicStates.size(),
+			.pDynamicStates = dynamicStates.data()
+	};
+
+	vk::PipelineRasterizationStateCreateInfo rasterizer{
+		.depthClampEnable = vk::False,
+			.rasterizerDiscardEnable = vk::False,
+			.polygonMode = vk::PolygonMode::eFill,
+			.cullMode = vk::CullModeFlagBits::eBack,
+			.frontFace = vk::FrontFace::eCounterClockwise,
+			.depthBiasEnable = vk::False,
+			.depthBiasSlopeFactor = 1.0f,
+			.lineWidth = 1.0f
+	};
+
+	vk::PipelineMultisampleStateCreateInfo multisampling{.rasterizationSamples=vk::SampleCountFlagBits::e1,.sampleShadingEnable=vk::False};
+	vk::PipelineColorBlendAttachmentState colorBlendAttachment{
+		.blendEnable    = vk::True,
+			.colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
+	};
+
+	// Alpha blending
+	colorBlendAttachment.blendEnable = vk::True;
+	colorBlendAttachment.srcColorBlendFactor = vk::BlendFactor::eSrcAlpha;
+	colorBlendAttachment.dstColorBlendFactor = vk::BlendFactor::eOneMinusSrcAlpha;
+	colorBlendAttachment.colorBlendOp = vk::BlendOp::eAdd;
+	colorBlendAttachment.srcAlphaBlendFactor = vk::BlendFactor::eOne;
+	colorBlendAttachment.dstAlphaBlendFactor = vk::BlendFactor::eZero;
+	colorBlendAttachment.alphaBlendOp = vk::BlendOp::eAdd;
+	vk::PipelineColorBlendStateCreateInfo colorBlending{.logicOpEnable = vk::False, .logicOp =  vk::LogicOp::eCopy, .attachmentCount = 1, .pAttachments =  &colorBlendAttachment };
+
+	vk::PipelineLayoutCreateInfo pipelineLayoutInfo{.setLayoutCount=1,
+		.pSetLayouts=&*descSetLayout,			
+		.pushConstantRangeCount=0
+	};
+
+	layout = vk::raii::PipelineLayout(dev, pipelineLayoutInfo);
+
+	vk::PipelineRenderingCreateInfo renderingCreateInfo{
+		.colorAttachmentCount=1,
+			.pColorAttachmentFormats=&surfaceFormat.format,
+			.depthAttachmentFormat=depthFormat
+	};
+
+	vk::PipelineDepthStencilStateCreateInfo depthStencil{
+		.depthTestEnable       = vk::True,
+			.depthWriteEnable      = vk::True,
+			.depthCompareOp        = vk::CompareOp::eLess,
+			.depthBoundsTestEnable = vk::False,
+			.stencilTestEnable     = vk::False
+	};
+
+	vk::GraphicsPipelineCreateInfo pipelineInfo{
+		.pNext = &renderingCreateInfo,
+			.stageCount = 2, .pStages = shaderStages,
+			.pVertexInputState = &vertInputInfo, .pInputAssemblyState = &inputAssembly,
+			.pViewportState = &viewportState, .pRasterizationState = &rasterizer,
+			.pMultisampleState = &multisampling,
+			.pDepthStencilState = &depthStencil, .pColorBlendState = &colorBlending,
+			.pDynamicState = &dynamicState, .layout = layout, 
+			.renderPass = nullptr,
+	};
+
+	pipeline = vk::raii::Pipeline(dev, nullptr, pipelineInfo);
+}
+
+std::shared_ptr<VulkanPipeline> PipelineManager::get(const PipelineKey &key)
+{
+	auto it = this->cache.find(key);
+	if(it != this->cache.end())
+		return it->second;
+
+	auto pipeline = std::make_shared<VulkanPipeline>(this->device, key, surfaceFormat, depthFormat, descSetLayout);
+	this->cache[key] = pipeline;
+	return pipeline;
+}
