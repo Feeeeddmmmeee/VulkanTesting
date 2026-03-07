125bb16c378ecd16284c2aa09c3771ec039e78eb
Feeeeddmmmeee <69256898+Feeeeddmmmeee@users.noreply.github.com>
2026-03-07 14:57:54 +0100

    moved the vertex struct to include/

---
+++ include/Vertex.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
+
+struct Vertex
+{
+	glm::vec3 pos;
+	glm::vec3 color;
+	glm::vec2 texCoord;
+
+	static vk::VertexInputBindingDescription getBindingDesc()
+	{
+		return {0, sizeof(Vertex), vk::VertexInputRate::eVertex};
+	}
+	static std::array<vk::VertexInputAttributeDescription, 3> getAttributeDescs()
+	{
+		return {
+			vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, pos)),
+				vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)),
+				vk::VertexInputAttributeDescription(2, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, texCoord))
+		};
+	}
+	bool operator==(const Vertex& other) const {
+		return pos == other.pos && color == other.color && texCoord == other.texCoord;
+	}
+};
+
+namespace std {
+    template<> struct hash<Vertex> {
+        size_t operator()(Vertex const& vertex) const {
+            return ((hash<glm::vec3>()(vertex.pos) ^
+                   (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+                   (hash<glm::vec2>()(vertex.texCoord) << 1);
+        }
+    };
+}
+
